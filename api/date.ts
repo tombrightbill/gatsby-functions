a41435963c8b8197f5b5eef75aa23313da5ba887
Tom Brightbill <tombrightbill@gmail.com>
2019-09-17 15:18:57 -0700

    new api call

--- api/date.ts
+++ api/date.ts
@@ -2,5 +2,6 @@ import { NowRequest, NowResponse } from '@now/node';
 
 export default (_req: NowRequest, res: NowResponse) => {
   const date = new Date().toString();
-  res.status(200).send(date);
+  const github = new Request({headers: {'content-type':'application/json'}}, 'https://api.github.com/search/issues?q=windows+label:bug+language:python+state:open&sort=created&order=asc')
+  res.status(200).send(github);
 };
